b6629057ab33e305e0f73d0da442d16420f7aa13
Varun A <varuna@adobe.com>
2024-08-07 17:26:22 +0530

    commit

--- blocks/carousel-block/carousel-block.js
+++ blocks/carousel-block/carousel-block.js
@@ -1,4 +1,4 @@
-// import { fetchLanguagePlaceholders } from '../../scripts/scripts.js';
+import { fetchLanguagePlaceholders } from '../../scripts/scripts.js';
 
 // function updateActiveSlide(slide) {
 //   const block = slide.closest('.carousel-block');
@@ -71,81 +71,82 @@
 //   });
 // }
 
-// function createSlide(row, slideIndex, carouselId) {
-//   const slide = document.createElement('li');
-//   slide.dataset.slideIndex = slideIndex;
-//   slide.setAttribute('id', `carousel-${carouselId}-slide-${slideIndex}`);
-//   slide.classList.add('carousel-slide');
+function createSlide(row, slideIndex, carouselId) {
+  const slide = document.createElement('li');
+  slide.dataset.slideIndex = slideIndex;
+  slide.setAttribute('id', `carousel-${carouselId}-slide-${slideIndex}`);
+  slide.classList.add('carousel-slide');
 
-//   row.querySelectorAll(':scope > div').forEach((column, colIdx) => {
-//     column.classList.add(`carousel-slide-${colIdx === 0 ? 'image' : 'content'}`);
-//     slide.append(column);
-//   });
+  row.querySelectorAll(':scope > div').forEach((column, colIdx) => {
+    column.classList.add(`carousel-slide-${colIdx === 0 ? 'image' : 'content'}`);
+    slide.append(column);
+  });
 
-//   const labeledBy = slide.querySelector('h1, h2, h3, h4, h5, h6');
-//   if (labeledBy) {
-//     slide.setAttribute('aria-labelledby', labeledBy.getAttribute('id'));
-//   }
+  const labeledBy = slide.querySelector('h1, h2, h3, h4, h5, h6');
+  if (labeledBy) {
+    slide.setAttribute('aria-labelledby', labeledBy.getAttribute('id'));
+  }
 
-//   return slide;
-// }
+  return slide;
+}
 
-// let carouselId = 0;
-// export default async function decorate(block) {
-//   carouselId += 1;
-//   block.setAttribute('id', `carousel-${carouselId}`);
-//   const rows = block.querySelectorAll(':scope > div');
-//   const isSingleSlide = rows.length < 2;
+let carouselId = 0;
+export default async function decorate(block) {
+  
+  carouselId += 1;
+  block.setAttribute('id', `carousel-${carouselId}`);
+  const rows = block.querySelectorAll(':scope > div');
+  const isSingleSlide = rows.length < 2;
 
-//   const placeholders = await fetchLanguagePlaceholders();
+  const placeholders = await fetchLanguagePlaceholders();
 
-//   block.setAttribute('role', 'region');
-//   block.setAttribute('aria-roledescription', placeholders.carousel || 'Carousel');
+  block.setAttribute('role', 'region');
+  block.setAttribute('aria-roledescription', placeholders.carousel || 'Carousel');
 
-//   const container = document.createElement('div');
-//   container.classList.add('carousel-slides-container');
+  const container = document.createElement('div');
+  container.classList.add('carousel-slides-container');
 
-//   const slidesWrapper = document.createElement('ul');
-//   slidesWrapper.classList.add('carousel-slides');
-//   block.prepend(slidesWrapper);
+  const slidesWrapper = document.createElement('ul');
+  slidesWrapper.classList.add('carousel-slides');
+  block.prepend(slidesWrapper);
 
-//   let slideIndicators;
-//   if (!isSingleSlide) {
-//     const slideIndicatorsNav = document.createElement('nav');
-//     slideIndicatorsNav.setAttribute('aria-label', placeholders.carouselSlideControls || 'Carousel Slide Controls');
-//     slideIndicators = document.createElement('ol');
-//     slideIndicators.classList.add('carousel-slide-indicators');
-//     slideIndicatorsNav.append(slideIndicators);
-//     block.append(slideIndicatorsNav);
+  let slideIndicators;
+  if (!isSingleSlide) {
+    const slideIndicatorsNav = document.createElement('nav');
+    slideIndicatorsNav.setAttribute('aria-label', placeholders.carouselSlideControls || 'Carousel Slide Controls');
+    slideIndicators = document.createElement('ol');
+    slideIndicators.classList.add('carousel-slide-indicators');
+    slideIndicatorsNav.append(slideIndicators);
+    block.append(slideIndicatorsNav);
 
-//     const slideNavButtons = document.createElement('div');
-//     slideNavButtons.classList.add('carousel-navigation-buttons');
-//     slideNavButtons.innerHTML = `
-//       <button type="button" class= "slide-prev" aria-label="${placeholders.previousSlide || 'Previous Slide'}"></button>
-//       <button type="button" class="slide-next" aria-label="${placeholders.nextSlide || 'Next Slide'}"></button>
-//     `;
+    const slideNavButtons = document.createElement('div');
+    slideNavButtons.classList.add('carousel-navigation-buttons');
+    slideNavButtons.innerHTML = `
+      <button type="button" class= "slide-prev" aria-label="${placeholders.previousSlide || 'Previous Slide'}"></button>
+      <button type="button" class="slide-next" aria-label="${placeholders.nextSlide || 'Next Slide'}"></button>
+    `;
 
-//     container.append(slideNavButtons);
-//   }
+    container.append(slideNavButtons);
+  }
 
-//   rows.forEach((row, idx) => {
-//     const slide = createSlide(row, idx, carouselId);
-//     slidesWrapper.append(slide);
+  rows.forEach((row, idx) => {
+    const slide = createSlide(row, idx, carouselId);
+    slidesWrapper.append(slide);
 
-//     if (slideIndicators) {
-//       const indicator = document.createElement('li');
-//       indicator.classList.add('carousel-slide-indicator');
-//       indicator.dataset.targetSlide = idx;
-//       indicator.innerHTML = `<button type="button" aria-label="${placeholders.showSlide || 'Show Slide'} ${idx + 1} ${placeholders.of || 'of'} ${rows.length}"></button>`;
-//       slideIndicators.append(indicator);
-//     }
-//     row.remove();
-//   });
+    if (slideIndicators) {
+      const indicator = document.createElement('li');
+      indicator.classList.add('carousel-slide-indicator');
+      indicator.dataset.targetSlide = idx;
+      indicator.innerHTML = `<button type="button" aria-label="${placeholders.showSlide || 'Show Slide'} ${idx + 1} ${placeholders.of || 'of'} ${rows.length}"></button>`;
+      slideIndicators.append(indicator);
+    }
+    row.remove();
+  });
 
-//   container.append(slidesWrapper);
-//   block.prepend(container);
+  container.append(slidesWrapper);
+  block.prepend(container);
 
 //   if (!isSingleSlide) {
 //     bindEvents(block);
 //   }
-// }
+}
